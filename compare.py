f39eccb46e171a7e6a3d5a7a1257fd7a6eca570d
iyx <palo282@centrum.sk>
2014-06-30 16:12:14 +0200

    commit

--- compare.py
+++ compare.py
@@ -4,10 +4,10 @@ c = 'output.txt'
 
 
 with open(a, 'r') as fa, open(b, 'r') as fb, open(c, 'w') as fc:
-    sa = set(fa.readlines())
-    sb = set(fb.readlines())
+    sa = set(x.strip() for x in fa.readlines())
+    sb = set(x.strip() for x in fb.readlines())
     sc = ()
-
+ 
     out = sa - sb
-
-    fc.writelines(sorted(out))
+ 
+    fc.writelines(x + '\n' for x in sorted(out))
